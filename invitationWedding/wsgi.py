a70b8749152990646709a217c1c6cee95cf7ddac
Luchezar31 <luchezar31@abv.bg>
2026-09-14 22:13:14 +0300

    adding gitignore

---
+++ invitationWedding/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for invitationWedding project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'invitationWedding.settings')
+
+application = get_wsgi_application()
